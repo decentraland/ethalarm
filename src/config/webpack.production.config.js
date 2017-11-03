66cc585ac4a18aa05e709f7784ad6a85a5fc26ef
Esteban Ordano <esteban@decentraland.org>
2017-11-03 01:03:01 -0300

    Add webpack config values

---
+++ src/config/webpack.production.config.js
@@ -0,0 +1,45 @@
+import webpack from 'webpack'
+import htmlPlugin from 'html-webpack-plugin'
+import path from 'path'
+
+export default {
+  devtool: 'source-map',
+  entry: {
+    'app': [
+      'babel-polyfill',
+      'react-hot-loader/patch',
+      'webpack-hot-middleware/client',
+      'webpack/hot/only-dev-server',
+      './webapp/main',
+    ]
+  },
+  output: {
+    filename: '[name].[hash].js',
+    publicPath: '/',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.s?css/,
+        use: [{
+            loader: 'style-loader'
+        }, {
+            loader: 'css-loader'
+        }, {
+            loader: 'sass-loader'
+        }]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve('__dirname', '../webapp')
+    }
+  },
+  plugins: [
+    new htmlPlugin({ template: 'public/index.html' }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
--- src/routes/alarms.js
+++ src/routes/alarms.js
@@ -2,79 +2,80 @@ import { Router } from 'express'
 import { Validator } from 'express-json-validator-middleware'
 
 import AlarmSchema from '../schemas/alarms'
-import Alarm from '../models/alarm'
 
-const router = Router()
 const validator = new Validator({ allErrors: true })
 const validate = validator.validate.bind(validator)
 
-const getResponseSchema = (response = {}) => {
+export default class AlarmRouter {
+  constructor(alarmService) {
+    this.alarm = alarmService
+  }
+
+  setupRouter(app) {
+    const router = Router()
+    router.post('/alarms', validate({ body: AlarmSchema }), this.postAlarm)
+    router.get('/alarms', validate({ body: AlarmSchema }), this.getAlarm)
+    router.delete('/alarms', validate({ body: AlarmSchema }), this.deleteAlarm)
+  }
+
+  get postAlarm() {
+    return (req, res) => {
+      this.alarm
+        .build(req.body)
+        .save()
+        .then(alarm => {
+          res.status(200).json(buildResponseSchema({ result: { id: alarm.id } }))
+        })
+        .catch(err => {
+          res.status(500).json(buildResponseSchema({ ok: false, error: err }))
+        })
+    }
+  }
+
+  get getAlarm() {
+    return (req, res) => {
+      this.alarm
+        .findById(req.params.id)
+        .then(alarm => {
+          if (!alarm) throw new Error(`Alarm ${req.params.id} not found`)
+          res.status(200).json(getResponseSchema({ alarm }))
+        })
+        .catch(err => {
+          res.status(404).json(
+            getResponseSchema({
+              ok: false,
+              error: err
+            })
+          )
+        })
+    }
+  }
+
+  get deleteAlarm() {
+    return (req, res) => {
+      this.alarm
+        .destroy({
+            where: { id: req.params.id }
+        })
+        .then(result => {
+          res.json(200)
+        })
+        .catch(err => {
+          res.json(500).json(
+            getResponseSchema({
+              ok: false,
+              error: err
+            })
+          )
+        })
+    }
+  }
+}
+
+const buildResponseSchema = (response = {}) => {
   return {
     ok: response.ok || true,
     error: response.error || '',
     result: response.result || {}
   }
 }
-
-router.post('/alarms', validate({ body: AlarmSchema }), (req, res) => {
-  Alarm
-    .build(req.body)
-    .save()
-    .then(alarm => {
-      console.log('Document Inserted OK')
-
-      res.status(200).json(
-        getResponseSchema({
-          result: {
-            id: alarm.id
-          }
-        })
-      )
-    })
-    .catch(err => {
-      res.status(500).json(
-        getResponseSchema({
-          ok: false,
-          error: err
-        })
-      )
-    })
-})
-
-router.get('/alarms/:id', (req, res) => {
-  Alarm
-    .findById(req.params.id)
-    .then(alarm => {
-      if (!alarm) return Promise.reject(alarm)
-      res.status(200).json(getResponseSchema({ alarm }))
-    })
-    .catch(err => {
-      res.status(404).json(
-        getResponseSchema({
-          ok: false,
-          error: err
-        })
-      )
-    })
-})
-
-router.delete('/alarms/:id', (req, res) => {
-    Alarm
-    .destroy({
-        where: { id: req.params.id }
-    })
-    .then(result => {
-      console.log('Alarm removed from database')
-      res.json(200)
-    })
-    .catch(err => {
-      res.json(500).json(
-        getResponseSchema({
-          ok: false,
-          error: err
-        })
-      )
-    })
-})
-
-export default router
